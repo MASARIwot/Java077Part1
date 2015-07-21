7a9fa3c8eeb3a527cb9331d65c493ff7cff6262e
user <user@user-VirtualBox>
2015-07-21 19:16:44 +0300

    new 2

--- Exercise09/src/com/luxoft/client/Client.java
+++ Exercise09/src/com/luxoft/client/Client.java
@@ -1,12 +1,28 @@
 package com.luxoft.client;
 
+import java.util.ArrayList;
+
+import com.luxoft.account.Account;
+
 public class Client {
 	private String name;
 	private Gender male;
+	private ArrayList<Account> accounts = new ArrayList<Account>();
 
 	public Client(String name, Gender male) {
 		this.name = name;
 		this.male = male;
+	}
+	
+	public Account getAccount(final int id) {
+		return  accounts.get(id);
+	}
+	public ArrayList<Account> getAccountList() {
+		return  accounts;
+	}
+
+	public void addAccount(final Account account) {
+		accounts.add(account);
 	}
 
 	public String getClientSalutation() {
